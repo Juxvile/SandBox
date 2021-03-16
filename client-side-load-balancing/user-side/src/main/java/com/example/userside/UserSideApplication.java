4af870c4f6705918056f1db3da65825f90b5cec5
Juxvile <andrugor1994@gmail.com>
2021-03-17 00:17:11 +0300

    commit

--- client-side-load-balancing/user-side/src/main/java/com/example/userside/UserSideApplication.java
+++ client-side-load-balancing/user-side/src/main/java/com/example/userside/UserSideApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +18,7 @@ public class UserSideApplication {
 
 	final RestTemplate restTemplate;
 
-	public UserSideApplication(RestTemplate restTemplate) {
+	public UserSideApplication(@Lazy RestTemplate restTemplate) {
 		this.restTemplate = restTemplate;
 	}
 
