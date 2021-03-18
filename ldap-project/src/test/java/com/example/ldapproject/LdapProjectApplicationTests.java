dc8f61d937b458720c511ef546eac68c3ff7745c
Juxvile <andrugor1994@gmail.com>
2021-03-18 13:15:43 +0300

    commit

---
+++ ldap-project/src/test/java/com/example/ldapproject/LdapProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ldapproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LdapProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
