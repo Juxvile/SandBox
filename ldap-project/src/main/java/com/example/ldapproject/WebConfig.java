2314836f3df3569d3e364d2de9e8410c1e604b4d
Juxvile <andrugor1994@gmail.com>
2021-03-18 13:37:57 +0300

    commit

--- ldap-project/src/main/java/com/example/ldapproject/WebConfig.java
+++ ldap-project/src/main/java/com/example/ldapproject/WebConfig.java
@@ -1,11 +1,17 @@
 package com.example.ldapproject;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.ldap.core.ContextSource;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.ldap.DefaultSpringSecurityContextSource;
+import org.springframework.security.ldap.userdetails.LdapUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
