52c3cb733d01fba4ce2c1de28dcea194f3d17eb9
Juxvile <andrugor1994@gmail.com>
2021-03-26 00:19:23 +0300

    commit

--- redis/src/main/java/com/example/redis/CoffeeConfiguration.java
+++ redis/src/main/java/com/example/redis/CoffeeConfiguration.java
@@ -3,32 +3,14 @@ package com.example.redis;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.jedis.JedisConnection;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.ReactiveRedisOperations;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class CoffeeConfiguration {
-
-    @Bean
-    JedisConnectionFactory jedisConnectionFactory() {
-        return new JedisConnectionFactory();
-    }
-
-
-    @Bean
-    public RedisTemplate<String, Coffee> redisTemplate() {
-        RedisTemplate<String, Coffee> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(jedisConnectionFactory());
-        return redisTemplate;
-    }
-
 
     @Bean
     ReactiveRedisOperations<String, Coffee> redisOperations(ReactiveRedisConnectionFactory factory) {
