0661fb9a54f6598b7767ed7681cab5df2c8a58fc
Juxvile <andrugor1994@gmail.com>
2021-03-26 23:29:08 +0300

    commit

---
+++ gemfire/src/main/java/com/example/gemfire/Person.java
@@ -0,0 +1,37 @@
+package com.example.gemfire;
+
+
+import lombok.Getter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.PersistenceConstructor;
+import org.springframework.data.gemfire.mapping.annotation.Region;
+
+import java.io.Serializable;
+
+@Region(value = "People")
+public class Person implements Serializable {
+
+    @Id
+    private final String name;
+
+    private final int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s is %d years old", getName(), getAge());
+    }
+
+    @PersistenceConstructor
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
